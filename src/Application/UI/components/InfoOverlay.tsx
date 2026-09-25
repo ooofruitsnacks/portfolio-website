9f1bf87ea8b37d416ec69540db85d0ed88839826
ooofruitsnacks <467487@pm.me>
2026-09-24 21:33:41 -0400

    secondary site initial commit

--- src/Application/UI/components/InfoOverlay.tsx
+++ src/Application/UI/components/InfoOverlay.tsx
@@ -6,8 +6,8 @@ interface InfoOverlayProps {
     visible: boolean;
 }
 
-const NAME_TEXT = 'Henry Heffernan';
-const TITLE_TEXT = 'Software Engineer';
+const NAME_TEXT = 'Owen Edwards';
+const TITLE_TEXT = 'Developer | Photographer | Dumbass';
 const MULTIPLIER = 1;
 
 const InfoOverlay: React.FC<InfoOverlayProps> = ({ visible }) => {
--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,18 +184,15 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://os.henryheffernan.com/';
-        /**
-         * Use dev server is query params are present
-         *
-         * Warning: This will not work unless the dev server is running on localhost:3000
-         * Also running the dev server causes browsers to freak out over unsecure connections
-         * in the iframe, so it will flag a ton of issues.
-         */
-        const urlParams = new URLSearchParams(window.location.search);
-        if (urlParams.has('dev')) {
-            iframe.src = 'http://localhost:3000/';
-        }
+        
+	const isLocal =
+	    window.location.hostname === 'localhost' ||
+	    window.location.hostname === '127.0.0.1';
+
+	iframe.src = isLocal
+	    ? 'http://localhost:3000/'
+	    : 'https://os.a-creative.studio/';
+
         iframe.style.width = this.screenSize.width + 'px';
         iframe.style.height = this.screenSize.height + 'px';
         iframe.style.padding = IFRAME_PADDING + 'px';
@@ -204,7 +201,7 @@ export default class MonitorScreen extends EventEmitter {
         iframe.className = 'jitter';
         iframe.id = 'computer-screen';
         iframe.frameBorder = '0';
-        iframe.title = 'HeffernanOS';
+        iframe.title = 'ToolyOS';
 
         // Add iframe to container
         container.appendChild(iframe);
